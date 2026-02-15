a6b1899f86f8f51b29ac1e4cbc4f6b0fcae503f8
Horyoshi <42314202+Cinecom@users.noreply.github.com>
2026-02-15 12:00:47 +0100

    added a new sound

--- shootyepgp.lua
+++ shootyepgp.lua
@@ -1008,7 +1008,7 @@ function sepgp:bidPrint(link, masterlooter, need, greed, bid)
   local msg = string.format(L["Click $MS or $PRIOOS or $OS for %s"], link)
   math.randomseed(GetTime() * 1000)
  
-  local randomSound = math.random(1, 28)
+  local randomSound = math.random(1, 29)
   if sepgp_sound == 1 then
 
       PlaySoundFile("Interface\\AddOns\\shootyepgp\\Sounds\\loot-" .. randomSound .. ".wav")
